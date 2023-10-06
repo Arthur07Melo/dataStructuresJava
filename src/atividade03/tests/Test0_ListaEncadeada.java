868f5477e842e4ba4166386c9db53e60b4141913
Arthur07Melo <aarthur00melo@gmail.com>
2023-10-06 18:31:10 -0300

    Adicionados testes para as seguintes funções de lista encadeada: isEmpty, Size, Remove, Search.

--- src/atividade03/tests/Test0_ListaEncadeada.java
+++ src/atividade03/tests/Test0_ListaEncadeada.java
@@ -8,27 +8,65 @@ import atividade03.ListaEncadeada_IF;
 
 public class Test0_ListaEncadeada {
 	
-	static ListaEncadeada_IF l ;
+	static ListaEncadeada_IF list ;
 		
 	@Before //Instanciando a Lista antes de cada teste
 	public void instanciaLista(){
-		l = new ListaEncadeada();
+		list = new ListaEncadeada();
 	}
 	
+
+	@Test
+	public void testIsEmpty(){
+		//TestCase: Lista inicia vazia
+		assertEquals(list.isEmpty(), true);
+		
+		//TestCase: isEmpty retorna false com lista não-vazia
+		list.insert(2);
+		assertEquals(list.isEmpty(), false);
+	}
+
+
 	@Test
 	public void testSize(){
-		assertEquals(0, l.size()); //com a lista vazia
+		//TestCase: Lista iniciza com size 0
+		assertEquals(0, list.size());
+
+		//Ao adicionar X (2) elementos, o size sera X
+		list.insert(3);
+		list.insert(5);
+		assertEquals(list.size(), 2);
 	}
+
+
+	@Test
+	public void testRemoveElement(){
+		//TestCase: Remover elemento válido
+		for(int i=1; i <= 5; i++){
+			list.insert(i);
+		}
+		list.remove(3);
+		assertEquals(4, list.size());
+	}
+
+	// Não retorna exceção de acordo com interface... mas era pra retornar :/
+	/*@Test(expected = Exception.class)
+	public void testRemoveElementException(){
+		//TestCase: Remover elemento inexistente na lista
+		list.remove(777);
+	}*/
+
 	
 	@Test
-	public void testRemove(){
-		for(int i=1; i <= 5; i++){
-			l.insert(i);
-		}
-		l.remove(3);
-		assertEquals(4, l.size());
+	public void testSearch() throws Exception{
+		//TestCase: Search encontra elemento
+		//para verificar se realmente está procurando elemento (que não esteja na primeira posição)
+		for(int i = 0; i < 5; i++){ list.insert(i); }
+		assertEquals(list.search(3), 3);
 	}
-	
-	//Outros testes ...
-	
+
+	@Test (expected = Exception.class)
+	public void testSearchException() throws Exception{
+		list.search(777);
+	}
 }
