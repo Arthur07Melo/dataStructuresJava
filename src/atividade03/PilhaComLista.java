fa67e54566f1d0c1fcbea8522bc630a5f4816a50
juliocesar710 <juliocandeia594@gmail.com>
2023-10-11 20:23:14 -0300

    Implementei a Pilha os métodos push,pop,top,IsEmpty,IsFull de acordo com a interface

--- src/atividade03/PilhaComLista.java
+++ src/atividade03/PilhaComLista.java
@@ -2,5 +2,60 @@ package atividade03;
 import atividade03.interfaces.Pilha_IF;
 
 public class PilhaComLista implements Pilha_IF {
+    private ListaEncadeada lista;
+    private int tamanho;
+    private Integer topo;
+    
+    
+    public PilhaComLista() {
+    
+    	this.lista = new ListaEncadeada();
+    	this.tamanho = 0;
+    	this.topo = null;
+    }
+    
+    
+    public void push(Integer element) {
+    	lista.insert(element);  
+    	this.topo = element;
+    	this.tamanho++;
+    }
+
+	@Override
+	public int pop() throws Exception {
+		if(this.tamanho==0) {
+			throw new Exception("Stack Underflow!! Não tem elementos aqui");
+		}
+		Integer old_topo = this.topo;
+		lista.remove(this.topo);
+		this.topo = lista.getData();
+		this.tamanho--;
+		return old_topo;
+	}
+
+	@Override
+	public int top() throws Exception {
+		if(this.tamanho==0) {
+			throw new Exception("Não tem elementos aqui");
+		}
+		return this.topo;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if(this.tamanho==0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		if(this.tamanho==10) {
+			return true;
+		}	
+		
+		return false;
+	}
     
 }
