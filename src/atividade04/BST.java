5f63158838ce7786f84fcc702d747b4d1012191d
Arthur07Melo <aarthur00melo@gmail.com>
2023-11-06 16:25:55 -0300

    Métodos da BST implementados e apropriadamente testados com sucesso.

--- src/atividade04/BST.java
+++ src/atividade04/BST.java
@@ -3,44 +3,92 @@ package atividade04;
 import atividade04.interfaces.BST_IF;
 
 public class BST implements BST_IF {
-    private int data;
+    private Integer data;
     private BST left;
     private BST right;
-    
-    public int getData() {
-        return data;
+
+    public BST(){
+        this.data = null;
+        this.left = null;
+        this.right = null;
     }
 
     @Override
     public void insert(int element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+        auxInsert(this, element);
     }
-    @Override
-    public int search(int element) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'search'");
-    }
-    @Override
-    public int[] preOrder() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'preOrder'");
-    }
-    @Override
-    public int[] order() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'order'");
-    }
-    @Override
-    public int[] postOrder() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'postOrder'");
-    }
-    @Override
-    public boolean isComplete() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isComplete'");
+    private void auxInsert(BST tree, int element){
+        if(tree.data == null) {
+            tree.data = element;
+            tree.left = new BST();
+            tree.right = new BST();
+        }
+        else if(element > tree.data) auxInsert(tree.right, element);
+        else auxInsert(tree.left, element);
     }
 
+    @Override
+    public int search(int element) throws Exception {
+        return auxSearch(this, element);
+    }
+    private int auxSearch(BST tree, int element) throws Exception {
+        if(tree.data == null) throw new Exception("Element not found");
+        else if(element == tree.data) return element;
+        else if(element > tree.data) return auxSearch(tree.right, element);
+        else return auxSearch(tree.left, element);
+    }
+
+    @Override
+    public int[] preOrder() {
+        int[] result = new int[this.size()];
+        auxPreOrder(this, result, 0);
+        return result;
+    }
+    private void auxPreOrder(BST tree, int[] result, int i){
+        if(tree.data == null) return;
+        result[i++] = tree.data;
+        auxPreOrder(tree.left, result, i);
+        auxPreOrder(tree.right, result, i + size(tree.left)); //soma-se mais a sizeLeft pois ela ja foi percorrida
+    }
+
+    @Override
+    public int[] order() {
+        int[] result = new int[this.size()];
+        auxOrder(this, result, 0);
+        return result;
+    }
+    private void auxOrder(BST tree, int[] result, int i){
+        if(tree.data == null) return;
+        auxOrder(tree.left, result, i);
+        result[i + size(tree.left)] = tree.data;
+        auxOrder(tree.right, result, i + size(tree.left) + 1);
+    }
     
+    @Override
+    public int[] postOrder() {
+        int[] result = new int[this.size()];
+        auxPostOrder(this, result, 0);
+        return result;
+    }
+    private void auxPostOrder(BST tree, int[] result, int i){
+        if(tree.data == null) return;
+        auxPostOrder(tree.left, result, i);
+        auxPostOrder(tree.right, result, i + size(tree.left));
+        result[i + size(tree.left) + size(tree.right)] = tree.data;
+    }
+
+    @Override
+    public boolean isComplete() {
+        if(this.size() == 0 || this.size() == 1) return true;
+        else if(size(this.left) == size(this.right)) return true;
+        return false;
+    }
+
+    private int size(){
+        return size(this);
+    }
+    private int size(BST tree){
+        if(tree.data == null) return 0;
+        return 1 + size(tree.left) + size(tree.right);
+    }
 }
