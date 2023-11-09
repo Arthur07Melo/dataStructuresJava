ba5a81ca36f96c033b2a8b4e2b3f3b3943faf6aa
Arthur07Melo <aarthur00melo@gmail.com>
2023-11-08 22:43:39 -0300

    Testes implementados para hashTable

---
+++ src/atividade04/TabelaHash.java
@@ -0,0 +1,35 @@
+package atividade04;
+
+import atividade04.interfaces.TabelaHash_IF;
+
+public class TabelaHash implements TabelaHash_IF {
+
+    public TabelaHash(int arraySize){
+        //te vira campeão, criei essa mímica (mock) de tabelaHash pra fazer os testes :)
+    }
+
+    @Override
+    public void insert(int element) {
+        // Te vira pra implementar champ
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void remove(int element) throws Exception {
+        // Te vira pra implementar champ
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int search(int element) throws Exception {
+        // Te vira pra implementar champ
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public String print() {
+        // Te vira pra implementar champ
+        throw new UnsupportedOperationException("Unimplemented method 'print'");
+    }
+    
+}
---
+++ src/atividade04/tests/Test0_TabelaHash.java
@@ -0,0 +1,84 @@
+package atividade04.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import atividade04.interfaces.TabelaHash_IF;
+import atividade04.TabelaHash;
+
+public class Test0_TabelaHash {
+    static TabelaHash_IF hashTable;
+
+    @Before
+    public void setupHashTable(){
+        hashTable = new TabelaHash(8);
+        
+    }
+
+    @Before
+    public void setupNumberInTable(){
+        hashTable.insert(5);
+        hashTable.insert(8);
+        hashTable.insert(0);
+        hashTable.insert(7);
+    }
+
+    @Test
+    public void testInsertSearch() throws Exception{
+        assertEquals(8, hashTable.search(8));
+        assertEquals(0, hashTable.search(0));
+
+    }
+
+    @Test (expected = Exception.class)
+    public void testInsertSearchException() throws Exception{
+        hashTable.search(16);
+        hashTable.search(15);
+
+    }
+
+    @Test
+    public void testRemoveHeadElement() throws Exception{
+        hashTable.remove(0);
+        assertThrows(Exception.class, () -> {
+            hashTable.search(0);
+        });
+
+    }
+
+    @Test
+    public void testRemoveMiddleListElement() throws Exception{
+        hashTable.insert(16);
+        hashTable.remove(8);
+        assertThrows(Exception.class, () -> {
+            hashTable.search(8);
+        });
+        //tenta remover o 8, sendo que a lista se encontra da seguinte forma:
+        /*
+         * index 0: 0 -> 8 -> 16
+         * deve ficar: 0 -> 16
+         */
+    }
+
+    @Test (expected = Exception.class)
+    public void testRemoveInexistentElement() throws Exception{
+        hashTable.remove(97);
+
+    }
+
+    @Test
+    public void testPrintTable() {
+        hashTable.insert(3);
+        hashTable.insert(4);
+        hashTable.insert(5);
+
+        String tablePrinted = hashTable.print();
+        String expected = 
+            "0: 0, 8\n1: \n2: \n3: 3\n4: 4\n5: 5, 5\n6: \n7: 7";
+
+        assertEquals(expected, tablePrinted);
+    }
+}
